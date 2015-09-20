7194cbe3deac79cc76638a4f14783ce61b75c39c
Chandler <creechch@umich.edu>
2015-09-20 18:22:11 -0400

    Added Insert_sorted

--- list.c
+++ list.c
@@ -8,8 +8,28 @@
 // `index` member in ascending order. Returns a pointer to the head of the list
 // after the insertion of the new element.
 list_t* insert_sorted(list_t* head, list_t* new_element) {
+
 	assert(head != NULL);
 	assert(new_element != NULL);
+	list_t* pointed = head;
+	int compared= pointed->index;
+	list_t* prev = NULL;
+
+	while(new_element->index > compared && pointed->next!=NULL){
+		prev= pointed;
+		pointed= pointed->next;
+		compared = pointed->index;
+	}
+
+	new_element->next = pointed;
+
+	if(prev!= NULL){
+		prev->next= new_element;
+	}
+
+	if(new_element->next == head){
+		head= new_element;
+	}
 
 	return head;
 }
@@ -18,6 +38,12 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+
+	list_t* newhead;
+
+	while(head->next!=NULL){
+
+	}
 
 	return head;
 }
